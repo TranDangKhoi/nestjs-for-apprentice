123af6ad8a915aa36486eb36a9b59765929f062c
Trần Đăng Khôi <baits16@gmail.com>
2025-12-18 17:02:25 +0700

    👍 slide through all of the methods

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { PostsService } from "./posts.service";
 
 @Controller("posts")
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(":id")
+  getPostById(@Param("id") id: string) {
+    return this.postsService.getPostById(id);
+  }
+
+  @Put(":id")
+  updatePost(@Param("id") id: string, @Body() body: { title: string }) {
+    return this.postsService.updatePost(id, body);
+  }
+
+  @Post()
+  createPost(@Body() body: { title: string }) {
+    return this.postsService.createPost(body);
+  }
+
+  @Delete(":id")
+  deletePost(@Param("id") id: string) {
+    return this.postsService.deletePost(id);
+  }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return "All posts";
+  }
+
+  getPostById(id: string) {
+    return "Post with id: " + id;
+  }
+
+  createPost(post: { title: string }) {
+    return "Post created with title: " + post.title;
+  }
+
+  updatePost(id, post: { title: string }) {
+    return "Post with id: " + id + " updated with title: " + post.title;
+  }
+
+  deletePost(id) {
+    return "Post with id: " + id + " deleted";
+  }
+}
